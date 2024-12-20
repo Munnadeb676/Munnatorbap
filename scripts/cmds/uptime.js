f4db5bbe48b010b4a392d61dd9e6077811e2692f
Munnadeb676 <boosmunna06@gmail.com>
2024-12-20 09:47:33 +0600

    Create uptime.js

---
+++ scripts/cmds/uptime.js
@@ -0,0 +1,28 @@
+module.exports = {
+  config: {
+    name: "uptime",
+    aliases: ["up", "upt"],
+    version: "1.0",
+    author: "XyryllPanget",
+    role: 0,
+    shortDescription: {
+      en: "Displays the uptime of the bot."
+    },
+    longDescription: {
+      en: "Displays the amount of time that the bot has been running for."
+    },
+    category: "box chat",
+    guide: {
+      en: "Use {p}uptime to display the uptime of the bot."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
+    api.sendMessage(`hello abul user ra bar bar command dile pod marbo  👽 bot coltece  ${uptimeString}.`, event.threadID);
+  }
+};
